4ed1d0ba98bee04669dc9c5202c6b2f310f19af1
HieuNguyen <hieunguyenbk93@gmail.com>
2022-06-01 09:46:52 +0700

    static list

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/data/todo.model.dart';
+
+final todoList = [
+  Todo(title: 'Do something 1'),
+  Todo(title: 'Do something 2'),
+  Todo(title: 'Do something 3'),
+];
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Text('Hello Flutter'),
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListView(
+            children: todoList.map(
+              (e) => Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Text(e.title)
+                )
+              )
+            ).toList(),
+          ),
+        ),
+      ),
     );
   }
 }
